1c0de034f0819377e7939176fc1345a46efbf303
TobMit <80719513+tobmit@users.noreply.github.com>
2022-11-02 22:27:38 +0100

    Naprogramovanie pripojenia a insertu

--- Cviko 06/DB.php
+++ Cviko 06/DB.php
@@ -2,5 +2,28 @@
 
 class DB
 {
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = new PDO('mysql:host=localhost;dbname=instac', "root", "heslo");
+    }
+
+    public function getAllPos()
+    {
+        $stm = $this->pdo->query("SELECT * FROM posts");
+
+        return $stm->fetchAll(PDO::FETCH_CLASS, Post::class);
+    }
+
+    public function storePost(Post $post)
+    {
+        $sql = "INSERT INTO posts (text) VALUES (?)";
+        $stmt= $this->pdo->prepare($sql);
+        $stmt->execute([$post->text]);
+    }
 
 }
