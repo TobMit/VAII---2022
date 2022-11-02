7e206ec38df09c4867293dffaa69561a58c38e8f
TobMit <80719513+tobmit@users.noreply.github.com>
2022-11-02 22:59:25 +0100

    Nejaké základy

--- Cviko 06/index.php
+++ Cviko 06/index.php
@@ -1,1 +1,26 @@
 <?php
+
+include "Post.php";
+include "DB.php";
+
+$db = new DB();
+
+?> <html>
+<head>
+
+
+</head>
+<body>
+
+<div class="posts-container">
+    jjflajldjfa
+    <?php foreach ($db->getAllPos() as $post) { ?>
+    <div class="post">
+        <?php $post->text ?>
+    </div>
+
+    <?php } ?>
+</div>
+
+</body>
+</html>
